db742e864ad93b3d1a038a6bbc4101bb0b1bb853
Mr Mysterius <25854814+MrMysterius@users.noreply.github.com>
2024-12-31 17:32:08 +0100

    fix db path

--- src/label.ts
+++ src/label.ts
@@ -6,7 +6,7 @@ import { DID, SIGNING_KEY } from './config.js';
 import { DELETE, LABELS, LABEL_LIMIT } from './constants.js';
 import logger from './logger.js';
 
-export const labelerServer = new LabelerServer({ did: DID, signingKey: SIGNING_KEY, dbPath: "./data" });
+export const labelerServer = new LabelerServer({ did: DID, signingKey: SIGNING_KEY, dbPath: "./data/labels.db" });
 
 export const label = (did: string, rkey: string) => {
   logger.info(`Received rkey: ${rkey} for ${did}`);
